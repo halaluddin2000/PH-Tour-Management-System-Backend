c1b2c85e324e31f3cf3c348aa39a8a3e5c53bece
halaluddin2000 <helaluddin7001@gmail.com>
2025-09-28 17:33:43 +0600

    user password hased

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -2,20 +2,25 @@ import httpStatus from "http-status-codes";
 import AppError from "../../errorHelpers/AppError";
 import { IAuthProvider, IUser } from "./user.interface";
 import { User } from "./user.model";
+import bcryptjs from "bcryptjs";
 
 const createUser = async (payload: Partial<IUser>) => {
-  const { email, ...rest } = payload;
+  const { email, password, ...rest } = payload;
   const isUserExist = await User.findOne({ email });
 
   if (isUserExist) {
     throw new AppError(httpStatus.BAD_REQUEST, "user already exist");
   }
+
+  const hashedPassword = await bcryptjs.hash(password as string, 10);
+
   const authProvider: IAuthProvider = {
     provider: "credential",
     providerId: email as string,
   };
   const user = await User.create({
     email,
+    password: hashedPassword,
     auths: [authProvider],
     ...rest,
   });
