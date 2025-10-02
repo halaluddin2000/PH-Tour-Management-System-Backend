6406a60a5deb22d0e42ebb9ac21cbe45ca9baf29
halaluddin2000 <helaluddin7001@gmail.com>
2025-10-02 19:20:55 +0600

    logout added

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -50,7 +50,29 @@ const getNewAccessToken = catchAsync(
     });
   }
 );
+const logout = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    res.clearCookie("accessToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+    res.clearCookie("refreshToken", {
+      httpOnly: true,
+      secure: false,
+      sameSite: "lax",
+    });
+
+    sedResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User Log Out Successfully",
+      data: null,
+    });
+  }
+);
 export const AuthControllers = {
   credentialsLogin,
   getNewAccessToken,
+  logout,
 };
