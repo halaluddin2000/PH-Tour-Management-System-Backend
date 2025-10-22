b63881ef0923c12fd9e159c86d29e4a374523324
halaluddin2000 <helaluddin7001@gmail.com>
2025-10-22 17:38:15 +0600

    slug tour or division added

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,6 +1,8 @@
 import { Router } from "express";
 import { UserRoutes } from "../app/modules/user/user.route";
 import { AuthRoutes } from "../app/modules/auth/auth.route";
+import { DivisionRoutes } from "../app/modules/division/division.route";
+import { TourRoutes } from "../app/modules/tour/tour.route";
 
 export const router = Router();
 
@@ -12,6 +14,14 @@ const moduleRoutes = [
   {
     path: "/auth",
     router: AuthRoutes,
+  },
+  {
+    path: "/division",
+    router: DivisionRoutes,
+  },
+  {
+    path: "/tour",
+    router: TourRoutes,
   },
 ];
 
